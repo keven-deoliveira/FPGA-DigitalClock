58da8a88d1e006722cfc89273ce7a28a080a2ee4
seifibrahim7 <74596289+seifibrahim7@users.noreply.github.com>
2020-11-25 23:41:18 +0200

    Update CURRENT_TIME.v

--- CURRENT_TIME.v
+++ CURRENT_TIME.v
@@ -21,6 +21,33 @@
 
 
 module CURRENT_TIME(
-
+    input [4:0] H_IN,
+    input [5:0] M_IN,
+    input [5:0] S_IN,
+    output reg [4:0] H_OUT,
+    output reg [5:0] M_OUT,
+    output reg [5:0] S_OUT,
+    input slowclk,
+    input reset
     );
+    
+    always@(posedge slowclk or posedge reset) begin
+    if(reset) begin 
+    H_OUT <= H_IN; M_OUT <= M_IN; S_OUT <= S_IN;
+    end 
+    
+    else begin 
+    if (S_OUT < 59) S_OUT <= S_OUT + 1;
+    else begin 
+        S_OUT <= 0; 
+        if (M_OUT < 59) M_OUT <= M_OUT + 1;
+        else begin
+            M_OUT <= 0; 
+            if (H_OUT < 23) H_OUT <= H_OUT +1;
+            else H_OUT <= 0;
+        end
+    end
+    end
+    end
+    
 endmodule
